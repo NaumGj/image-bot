c3eb34d7c43dcabb41b0d72ce94201e66e8f9c80
NAUM-PC\Naum <naum.gjorgjeski@tum.de>
2017-11-18 14:37:53 +0100

    Fix image upload 6

--- app.js
+++ app.js
@@ -32,36 +32,17 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
-    function (session) {
-        builder.Prompts.text(session, "Alou... What's your name?");
-    },
     function (session, results) {
-        session.userData.name = results.response;
-		session.send("Hi " + results.response);
 		builder.Prompts.attachment(session, "Upload a picture for me.");
 		//builder.Prompts.number(session, "Hi " + results.response + ", please upload a picture?!");
         // builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
     },
-    /*function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
-    },*/
 	function (session, results) {
-		console.log(results);
-		console.log(session.message);
 		var msg = session.message;
 		if (msg.attachments && msg.attachments.length > 0) {
 			// Echo back attachment
 			var attachment = msg.attachments[0];
 			session.send("You sent: " + JSON.stringify(attachment));
-			session.send(attachment.name);
-			session.send(attachment.size);
 			session.send({
 				text: "You sent:",
 				attachments: [
