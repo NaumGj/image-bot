2fc81887da72ca1c5dbe98d52ed3a8401356bd1e
René Birkeland <renesbirkeland@gmail.com>
2017-11-18 19:07:59 +0100

    Add mood of people in image

--- processImageAPI.js
+++ processImageAPI.js
@@ -41,26 +41,49 @@ function findFaces(url, analyzeData) {
     });
 }
 
-function createDescription(url) {
-    analyzeImage("https://www.bigdipper.in/wp-content/uploads/2017/07/friends.jpg");
-    //findFaces("https://www.bigdipper.in/wp-content/uploads/2017/07/friends.jpg");
-
-}
-
 function handleResponse(analyzeData, faceData) {
     console.log(analyzeData);
     console.log(faceData);
-    //var data2 = eval('('+ faceData + ')');
-    //console.log(JSON.stringify(analyzeData.description))
 
-    //var emotions = faceData
+    //Handle Emotions
 
-    var json = JSON.parse('{ "NumOfPeople":'+ faceData.length + ', "caption":'+ JSON.stringify(analyzeData.description.captions) +', "tags":'+ JSON.stringify(analyzeData.description.tags) +'}'); 
+    var emotions = {"anger":0, "contempt":0, "disgust":0,"fear":0,"happiness":0,"neutral":0,"sadness":0,"surprise":0};
+
+    for(var i = 0; i<faceData.length;i++) {
+        var dict = faceData[i].faceAttributes.emotion;
+
+        // Create items array
+        var items = Object.keys(dict).map(function(key) {
+            return [key, dict[key]];
+        });
+
+        // Sort the array based on the second element
+        items.sort(function(first, second) {
+            return second[1] - first[1];
+        });
+
+        emotions[items[0]]++ 
+    }
+
+    // Create items array
+    var items = Object.keys(emotions).map(function(key) {
+        return [key, dict[key]];
+    });
+
+    // Sort the array based on the second element
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+
+
+    // Handle gender
+
+
+    var json = JSON.parse('{ "NumOfPeople":'+ faceData.length + ', "caption":'+ JSON.stringify(analyzeData.description.captions[0].text) +', "tags":'+ JSON.stringify(analyzeData.description.tags) +', "mood":"' +items[0][0]+ '"}'); 
 
     console.log(json)
 
-
 }
 
-createDescription("");
+analyzeImage("https://peopledotcom.files.wordpress.com/2017/11/serena-williams-wedding-2.jpg?crop=0px%2C0px%2C2000px%2C1334.2318059299px&resize=742%2C495");
 
