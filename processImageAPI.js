6ad481ab4e18a47a76dc98ae12bef801445a8026
René Birkeland <renesbirkeland@gmail.com>
2017-11-18 15:49:57 +0100

    Add face detection

--- processImageAPI.js
+++ processImageAPI.js
@@ -2,18 +2,8 @@
 var Client = require('node-rest-client').Client;
 
 var client = new Client();
-var url = "https://www.scienceabc.com/wp-content/uploads/2017/02/Thailand-beach-sand.jpg"
 
-// set content-type header and data as json in args parameter 
-var args = {
-    data: '{"url": ' + '"' + url + '"}',
-    headers: { "Content-Type": "application/json", "Ocp-Apim-Subscription-Key": "1c5abc7f2dd84c37aea599f0839d9048"}
-};
-
-function processImage() {
-    // **********************************************
-    // *** Update or verify the following values. ***
-    // **********************************************
+function processImage(url) {
 
     // Replace the subscriptionKey string value with your valid subscription key.
     var subscriptionKey = "1c5abc7f2dd84c37aea599f0839d9048";
@@ -22,6 +12,12 @@ function processImage() {
             "visualFeatures": "Categories,Description,Color",
             "details": "",
             "language": "en",
+    };
+
+    // set content-type header and data as json in args parameter 
+    var args = {
+        data: '{"url": ' + '"' + url + '"}',
+        headers: { "Content-Type": "application/json", "Ocp-Apim-Subscription-Key": subscriptionKey}
     };
 
     // Replace or verify the region.
@@ -33,7 +29,6 @@ function processImage() {
     // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
     // a free trial subscription key, you should not need to change this region.
     var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Categories,Description,Color";
-    console.log(uriBase);
 
     client.post(uriBase, args, function (data, response) {
         // parsed response body as js object 
@@ -43,44 +38,33 @@ function processImage() {
     });
 }
 
-processImage();
+function findFaces(url) {
+    
+    // Replace the subscriptionKey string value with your valid subscription key.
+    var subscriptionKey = "ab5e7421bd1646a39fe1bd37be99cc42";
 
-    /*// registering remote methods 
-    client.registerMethod("postMethod", "http://remote.site/rest/json/method", "POST");
+    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+    
+    var params = {
+        "returnFaceId": "true",
+        "returnFaceLandmarks": "false",
+        "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+    };
 
-    client.methods.postMethod(args, function (data, response) {
+    var args = {
+        data: '{"url": ' + '"' + url + '"}',
+        headers: { "Content-Type": "application/json", "Ocp-Apim-Subscription-Key": subscriptionKey}
+    };
+
+    client.post(uriBase, args, function (data, response) {
         // parsed response body as js object 
         console.log(data);
         // raw response 
-        console.log(response);
-    });*/
+        //console.log(response);
+    });
 
-        /*// Perform the REST API call.
-        $.ajax({
-            url: uriBase + "?" + $.param(params),
 
-            // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            },
+}
 
-            type: "POST",
-
-            // Request body.
-            data: '{"url": ' + '"' + sourceImageUrl + '"}',
-        })
-
-        .done(function(data) {
-            // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
-        })
-
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Display error message.
-            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
-            errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
-            alert(errorString);
-        });
-    };
-*/
+processImage("https://www.bigdipper.in/wp-content/uploads/2017/07/friends.jpg");
+findFaces("https://www.bigdipper.in/wp-content/uploads/2017/07/friends.jpg");
